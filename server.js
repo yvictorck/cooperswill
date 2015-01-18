deab0f565b8e344a33d201ed60b2f2a4c804d362
Victor Chen <yvictorck@gmail.com>
2015-01-17 23:46:41 -0500

    asdf

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-//app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
-app.use(gzippo.staticGzip('' + __dirname + '/public'));
+// app.use(gzippo.staticGzip('' + __dirname + '/public'));
 // app.use('/', routes);
 app.use('/nerds', nerds);
 
